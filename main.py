931799ff1fb01b113d0ed9eb6d33d9e67527abcb
wiggapony0925 <ninjeff06@gmail.com>
2023-10-19 17:21:05 -0400

    full version

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from website import create_app
 
-app = create_app
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)   # Every time we make a change it'll rerun the server 
     
     
-      
+     
