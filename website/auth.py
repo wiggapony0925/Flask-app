f77c024a83055312d6f3074ddfd6049f10ef89a1
wiggapony0925 <ninjeff06@gmail.com>
2023-10-19 20:12:37 -0400

    setout routes

--- website/auth.py
+++ website/auth.py
@@ -4,3 +4,24 @@ from flask import Blueprint
 
 auth = Blueprint("auth", __name__)
 
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+
+#/login route
+@auth.route('/login')
+def login():
+    return "<h1>LoginPage</h1>"
+
+
+#logout
+@auth.route("/logout")
+def logout():
+    return "<h1>Logout</h1>"
+
+
+#Register route
+@auth.route("/sign-up")
+def sign_up():
+    return "<h1>REGISTER</h1>"
