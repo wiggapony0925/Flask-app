e0efb6d372a00e643580db6c92d50e5da3118ea3
wiggapony0925 <ninjeff06@gmail.com>
2023-10-22 00:01:37 -0400

    user model

--- website/models.py
+++ website/models.py
@@ -2,8 +2,11 @@ from . import db
 from flask_login import UserMixin
 
 class User(db.model, UserMixin):
-    pass 
-
-
+   id = db.Column(db.Integer, primary_key=True, nullable=False)
+   email = db.Column(db.String(80), unique=True, nullable=False)
+   password = db.Colum(db.String(40), nullable=False)
+   budget = db.Colum(db.Integer, nullable=False)
+   
+   
 class VendingMachine(db.model):
     pass 
