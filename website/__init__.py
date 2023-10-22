d2ca2685176d0b846b09034795e4b0e837e5760d
wiggapony0925 <ninjeff06@gmail.com>
2023-10-21 23:37:02 -0400

    basic DB connection

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 import os 
 
+db = SQLAlchemy
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-
+DB_NAME = os.getenv("DB_NAME")
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['SQLALCHEMY_DATABASE_IRL'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
