a2e310d84473989de6ec8dddbb5a7fe840041071
wiggapony0925 <ninjeff06@gmail.com>
2023-10-22 23:47:18 -0400

    auth users cant acess /log and /sign-up

--- website/auth.py
+++ website/auth.py
@@ -7,7 +7,6 @@ from .models import User, db
 auth = Blueprint('auth', __name__)
 
 def is_valid_email(email):
-    
     pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
     return re.match(pattern, email)
 
@@ -16,6 +15,9 @@ def is_strong_password(password):
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('views.home'))
+
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
@@ -39,12 +41,14 @@ def logout():
 
 @auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
+    if current_user.is_authenticated:
+        return redirect(url_for('views.home'))
+
     if request.method == 'POST':
         email = request.form.get('email')
         firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        
     
         user = User.query.filter_by(email=email).first()
         if user:
@@ -58,7 +62,6 @@ def sign_up():
         elif password1 != password2:
             flash('Passwords do not match', category='error')
         else:
-    
             hashed_password = generate_password_hash(password1, method='sha256')
             new_user = User(email=email, firstName=firstName, password=hashed_password)
             db.session.add(new_user)
