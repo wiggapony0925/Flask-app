dbc734105d850e8d6709b121dc65645131139f09
wiggapony0925 <ninjeff06@gmail.com>
2023-10-23 18:36:56 -0400

    created start code

--- website/vending_Machine.py
+++ website/vending_Machine.py
@@ -6,14 +6,15 @@ from .models import Transaction, Note, VendingMachine, db
 
 vending_machine_bp = Blueprint('vending_machine_bp', __name__)
 
+#load the html under the selected machine
 @vending_machine_bp.route('/vending_machine_dashboard/<int:machine_id>')
 @login_required
 def vending_machine_dashboard(vending_machine_id):
     # Your view logic here
     return render_template("vending_machine_dashboard.html", machine_id=vending_machine_id)
 
-
-@vending_machine_bp.route('/vending_machine/<int:id>/notes', methods=['POST', 'GET'])
+#create a note
+@vending_machine_bp.route('/vending_machine_dashboard/<int:id>/notes', methods=['POST', 'GET'])
 @login_required
 def create_notes(id):
     if request.method == 'POST':
@@ -31,3 +32,19 @@ def create_notes(id):
 
     notes = Note.query.filter_by(vending_machine_id=id).all()
     return render_template('das.html', notes=notes)  
+
+#edit the note
+@vending_machine_bp.route('/vending_machine/<int:machine_id>/notes/<int:note_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_note(machine_id, note_id):
+    pass
+
+#delete the note
+@vending_machine_bp.route('/vending_machine/<int:machine_id>/notes/<int:note_id>/edit', methods=['GET', 'POST'])
+@login_required
+def delete_note(machine_id, note_id):
+    pass
+
+
+'''TRANSCATIONS'''
+
