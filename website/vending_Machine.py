74f40f191866cad48c05afed55ea0f6839ef9b67
wiggapony0925 <ninjeff06@gmail.com>
2023-10-24 16:32:28 -0400

    WORKING VERSION fixed routes

--- website/vending_Machine.py
+++ website/vending_Machine.py
@@ -1,19 +1,17 @@
-from flask import Blueprint, render_template, Blueprint, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-#ROUTES WITH IN MY APP
-
 from .models import Transaction, Note, VendingMachine, db
 
 vending_machine_bp = Blueprint('vending_machine_bp', __name__)
 
-#load the html under the selected machine
+# Load the HTML under the selected machine
 @vending_machine_bp.route('/vending_machine_dashboard/<int:machine_id>')
 @login_required
-def vending_machine_dashboard(vending_machine_id):
+def vending_machine_dashboard(machine_id):
     # Your view logic here
-    return render_template("vending_machine_dashboard.html", machine_id=vending_machine_id)
+    return render_template("vending_machine_dashboard.html", machine_id=machine_id)
 
-#create a note
+# Create a note
 @vending_machine_bp.route('/vending_machine_dashboard/<int:id>/notes', methods=['POST', 'GET'])
 @login_required
 def create_notes(id):
@@ -23,28 +21,23 @@ def create_notes(id):
         if len(note_content) < 2:
             flash("Note is too short!", category="error")
         else:
-            new_note = Note(data=note_content, user_id=current_user.id, machine_id=id) 
+            new_note = Note(data=note_content, user_id=current_user.id, vending_machine_id=id) 
             db.session.add(new_note)
             db.session.commit()
-            flash("The note has been added sucessfully", category='error')
+            flash("The note has been added successfully", category='success')
             return redirect(url_for('vending_machine_bp.create_notes', id=id)) 
-
 
     notes = Note.query.filter_by(vending_machine_id=id).all()
     return render_template('das.html', notes=notes)  
 
-#edit the note
+# Edit the note
 @vending_machine_bp.route('/vending_machine/<int:machine_id>/notes/<int:note_id>/edit', methods=['GET', 'POST'])
 @login_required
 def edit_note(machine_id, note_id):
     pass
 
-#delete the note
-@vending_machine_bp.route('/vending_machine/<int:machine_id>/notes/<int:note_id>/edit', methods=['GET', 'POST'])
+# Delete the note
+@vending_machine_bp.route('/vending_machine/<int:machine_id>/notes/<int:note_id>/delete', methods=['POST'])
 @login_required
 def delete_note(machine_id, note_id):
     pass
-
-
-'''TRANSCATIONS'''
-
--- website/views.py
+++ website/views.py
@@ -1,25 +1,23 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from datetime import datetime
-#ROUTES WITH IN MY APP
 
 from .models import VendingMachine, db
+
 views = Blueprint("views", __name__)
 
 @views.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-#display the current users vending machines
+# Display the current user's vending machines
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     user_vending_machine = VendingMachine.query.filter_by(user_id=current_user.id).all()
-    
     return render_template("home.html", user=current_user, vending_machines=user_vending_machine)
 
-
-#create
+# Create a vending machine
 @views.route('/create_vending_machine', methods=['POST', 'GET'])
 @login_required
 def create_vending_machine():
@@ -28,64 +26,57 @@ def create_vending_machine():
         location = request.form.get("location")
         date_str = request.form.get('date') 
         budget = float(request.form.get('budget'))
-        total_sales = 0  # Default sales would start at 0
-
+        total_sales = 0  
         if not machine_name or not budget:
             flash("Please fill in all fields", category='error')
         else:
-            
             date = datetime.strptime(date_str, '%Y-%m-%d').date()
-            
             new_vending_machine = VendingMachine(
                 machine_name=machine_name,
                 location=location,
                 date=date,
                 budget=budget,
                 total_sales=total_sales,
-                user_id=current_user.id  # Associate with the currently logged-in user
+                user_id=current_user.id  
             )
-
             db.session.add(new_vending_machine)
             db.session.commit()
-
             flash(f"Successfully added {machine_name} to your portfolio!", category='success')
             return redirect(url_for('views.home'))
 
     return render_template("/createMachine.html", title="Create a New Machine", form=VendingMachine())
 
-
-#edit 
+# Edit a vending machine
 @views.route('/edit_vending_machine/<int:vending_machine_id>', methods=['GET', 'POST'])
 @login_required
 def edit_vending_machine(vending_machine_id):
-    vending_machine = VendingMachine.query.get(vending_machine_id)
-    
-    # Makes sure there's a vending machine
-    if vending_machine is None or vending_machine.user_id != current_user.id:
+    vending_machine = VendingMachine.query.filter_by(id=vending_machine_id, user_id=current_user.id).first()
+
+    if vending_machine is None:
         flash('Vending machine not found', 'error')
         return redirect(url_for('views.home'))
-    
+
     if request.method == "POST":
+        # Get the updated values from the form
         machine_name = request.form.get('machine_name')
         location = request.form.get('location')
         budget = float(request.form.get('budget'))
-        date = request.form.get('date')
-
-        # Update the vending machine's attributes
+        date_str = request.form.get('date')
+        
+        # Update the vending machine attributes
         vending_machine.machine_name = machine_name
         vending_machine.location = location
         vending_machine.budget = budget
-        vending_machine.date = date
-        
-        db.session.commit()  # Use db.session instead of db
+        vending_machine.date = datetime.strptime(date_str, '%Y-%m-%d').date()
+
+        db.session.commit()
 
         flash('Vending machine settings updated successfully', 'success')
         return redirect(url_for('views.home'))
 
-    return render_template("edit_vending_machine.html", vending_machine=vending_machine)
+    return render_template("edit_vending_machine.html", vending_machine=vending_machine, user=current_user)
 
-
-#delete
+# Delete a vending machine
 @views.route('/delete_vending_machine/<int:vending_machine_id>', methods=['POST'])
 @login_required
 def delete_vending_machine(vending_machine_id):
@@ -101,5 +92,3 @@ def delete_vending_machine(vending_machine_id):
 
     flash('Vending machine deleted successfully', 'success')
     return redirect(url_for('views.home'))
-
-
